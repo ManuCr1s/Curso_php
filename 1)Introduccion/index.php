0756dff7d54aaa78db6552a32b2d5e83b1b36f04
Manuel <Manu_NC@outlook.com>
2020-08-30 18:39:48 +0200

    Leccion 2 Configuarion Server

---
+++ 1)Introduccion/index.php
@@ -0,0 +1,12 @@
+<?php
+//                                  INTRODUCCION A PHP
+/*
+    --Podemos modificar algunas caracteristicas del SERVER en:
+        PHP.INI
+    --Modificar algunos comandos como el tiempo maximo de respuesta.
+    --Modificar el tiempo maximo de carga.
+    --Modificar la etiqueta de apertura.
+    --Modificar el Display_errors, que nos muestra mensajes de error.
+    --Modificar la cantidad de memoria a consumir por un script.
+*/
+?>
