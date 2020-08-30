ca96373c66562e78a93aa9c1c971eee93891db24
Manuel <Manu_NC@outlook.com>
2020-08-30 19:26:52 +0200

    Leccion 3 Tipos de Datos

---
+++ 2)Datos/index.php
@@ -0,0 +1,15 @@
+<?php
+/*Tipos de Dato: Según su valor*/
+    $entero = 150;
+    $decimal = 10.541;
+    $cadena = "Hola Mundo";
+    $booleano = true;
+    $array1 = ["1",2,false,"Hola",2.5];
+    $array2 = array("Lunes",true,2,2.54);
+/*Tipos de Dato: Según su funcion*/
+    //VARIABLES.-Se antepone el simbolo de dolar.
+            $variable=15;
+    //CONSTANTES.-Se utiliza la funcion DEFINE();
+            define('CONSTANTE',5);
+            echo CONSTANTE;
+?>
