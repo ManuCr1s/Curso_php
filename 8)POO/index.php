79dcbb4df05618b30520ebead4f4fd2d6927c0c5
Manuel <Manu_NC@outlook.com>
2020-08-31 19:49:34 +0200

    Leccion 9 POO

---
+++ 8)POO/index.php
@@ -0,0 +1,7 @@
+<?php
+//Creacion de una clase
+class Persona{
+    public function Imprimir($nombre,$apellido){
+        echo "Hola ".$nombre. ' '.$apellido;
+    }
+}
---
+++ 8)POO/objeto.php
@@ -0,0 +1,4 @@
+<?php
+require_once "index.php";
+$objPersona = new Persona();
+$objPersona->Imprimir('Manuel','Cristobal Neyra');
